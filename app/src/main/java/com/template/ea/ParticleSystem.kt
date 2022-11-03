89adeb5aed06109aaf22c37ebe24c2bf8cb3311e
benigumocom <benigumo.com@gmail.com>
2022-11-04 04:12:43 +0900

    Updates

--- app/src/main/java/com/template/ea/ParticleSystem.kt
+++ app/src/main/java/com/template/ea/ParticleSystem.kt
@@ -2,70 +2,57 @@ package com.template.ea
 
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import kotlin.math.pow
 
-class ParticleSystem(
-  view: SimulationView,
-  mDstWidth: Int,
-  mDstHeight: Int
-) {
+class ParticleSystem(view: SimulationView, width: Int, height: Int) {
 
-  private var mLastT = 0L
-
-  val mBalls = arrayOfNulls<Particle>(NUM_PARTICLES)
+  private var last = 0L
+  val balls = List(NUM_PARTICLES) { Particle(view.context) }
 
   init {
-    val context = view.context
-    for (i in mBalls.indices) {
-      mBalls[i] = Particle(context)
-      mBalls[i]?.setBackgroundResource(R.drawable.ball)
-      mBalls[i]?.setLayerType(FrameLayout.LAYER_TYPE_HARDWARE, null)
-      view.addView(mBalls[i], ViewGroup.LayoutParams(mDstWidth, mDstHeight))
+    balls.forEach { ball ->
+      ball.apply {
+        setBackgroundResource(R.drawable.ball)
+        setLayerType(FrameLayout.LAYER_TYPE_HARDWARE, null)
+      }
+      view.addView(ball, ViewGroup.LayoutParams(width, height))
     }
   }
 
-  private fun updatePositions(sx: Float, sy: Float, timestamp: Long) {
-    //println("$timestamp $sx, $sy") OK
-    if (mLastT != 0L) {
-      val dT = (timestamp - mLastT).toFloat() / 1000f
-      val count = mBalls.size
-      for (i in 0 until count) {
-        val ball = mBalls[i]
-        ball!!.computePhysics(sx, sy, dT)
+  private fun updatePositions(sx: Float, sy: Float, current: Long) {
+    if (last != 0L) {
+      val delta = (current - last).toFloat() / 1000f
+      balls.forEach { ball ->
+        ball.computePhysics(sx, sy, delta)
       }
     }
-    mLastT = timestamp
+    last = current
   }
 
-  fun update(
-    sx: Float,
-    sy: Float,
-    now: Long,
-    mHorizontalBound: Float,
-    mVerticalBound: Float
-  ) {
+  fun update(sx: Float, sy: Float, now: Long, horizontalBound: Float, verticalBound: Float) {
 
     updatePositions(sx, sy, now)
 
-    val NUM_MAX_ITERATIONS = 10
+    val maxIterations = 10
 
     var more = true
-    val count = mBalls.size
+    val count = balls.size
     var k = 0
-    while (k < NUM_MAX_ITERATIONS && more) {
+    while (k < maxIterations && more) {
       more = false
       for (i in 0 until count) {
-        val curr = mBalls[i]
+        val curr = balls[i]
         for (j in i + 1 until count) {
-          val ball = mBalls[j]
-          var dx = ball!!.posX - curr!!.posX
+          val ball = balls[j]
+          var dx = ball.posX - curr.posX
           var dy = ball.posY - curr.posY
           var dd = dx * dx + dy * dy
-          if (dd <= SimulationView.sBallDiameter2) {
+          if (dd <= SimulationView.DIAMETER.pow(2)) {
             dx += (Math.random().toFloat() - 0.5f) * 0.0001f
             dy += (Math.random().toFloat() - 0.5f) * 0.0001f
             dd = dx * dx + dy * dy
             val d = Math.sqrt(dd.toDouble()).toFloat()
-            val c = 0.5f * (SimulationView.sBallDiameter - d) / d
+            val c = 0.5f * (SimulationView.DIAMETER - d) / d
             val effectX = dx * c
             val effectY = dy * c
             curr.posX -= effectX
@@ -75,22 +62,10 @@ class ParticleSystem(
             more = true
           }
         }
-        curr!!.resolveCollisionWithBounds(mHorizontalBound, mVerticalBound)
+        curr.resolveCollisionWithBounds(horizontalBound, verticalBound)
       }
       k++
     }
-  }
-
-  fun getParticleCount(): Int {
-    return mBalls.size
-  }
-
-  fun getPosX(i: Int): Float {
-    return mBalls[i]!!.posX
-  }
-
-  fun getPosY(i: Int): Float {
-    return mBalls[i]!!.posY
   }
 
   companion object {
--- app/src/main/java/com/template/ea/SimulationView.kt
+++ app/src/main/java/com/template/ea/SimulationView.kt
@@ -18,41 +18,35 @@ class SimulationView(
   context: Context
 ) : FrameLayout(context), SensorEventListener {
 
-  private val mDstWidth: Int
-  private val mDstHeight: Int
-  private lateinit var mAccelerometer: Sensor
-  //private var mLastT: Long = 0
-  private val mXDpi: Float
-  private val mYDpi: Float
-  private val mMetersToPixelsX: Float
-  private val mMetersToPixelsY: Float
-  private var mXOrigin = 0f
-  private var mYOrigin = 0f
-  private var mSensorX = 0f
-  private var mSensorY = 0f
-  private var mHorizontalBound = 0f
-  private var mVerticalBound = 0f
-  private val mParticleSystem: ParticleSystem
+  private val dstWidth: Int
+  private val dstHeight: Int
+  private lateinit var accelerometer: Sensor
+  private val xDpi: Float
+  private val yDpi: Float
+  private val metersToPixelsX: Float
+  private val metersToPixelsY: Float
+  private var xOrigin = 0f
+  private var yOrigin = 0f
+  private var sensorX = 0f
+  private var sensorY = 0f
+  private var horizontalBound = 0f
+  private var verticalBound = 0f
+  private val particleSystem: ParticleSystem
 
   private var mSensorManager: SensorManager? = null
   private var mDisplay: Display? = null
   private var mWindowManager: WindowManager? = null
 
   fun startSimulation(sensorManager: SensorManager, windowManager: WindowManager,display: Display) {
-
-    println(">>>>>>>>>>>>")
-
     mSensorManager = sensorManager
     mWindowManager = windowManager
     mDisplay = display
 
-    mAccelerometer = mSensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-    mSensorManager!!.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME)
+    accelerometer = mSensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+    mSensorManager!!.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME)
   }
 
   fun stopSimulation() {
-    println("<<<<<<<<<<<")
-
     mSensorManager?.unregisterListener(this)
 
     mSensorManager = null
@@ -76,77 +70,64 @@ class SimulationView(
     }
 
 
-    mXDpi = 393F //metrics.xdpi
-    mYDpi = 800F //metrics.ydpi
-    mMetersToPixelsX = mXDpi / 0.0254f
-    mMetersToPixelsY = mYDpi / 0.0254f
+    xDpi = 393F //metrics.xdpi
+    yDpi = 800F //metrics.ydpi
+    metersToPixelsX = xDpi / 0.0254f
+    metersToPixelsY = yDpi / 0.0254f
 
     // rescale the ball so it's about 0.5 cm on screen
-    mDstWidth = (sBallDiameter * mMetersToPixelsX + 0.5f).toInt()
-    mDstHeight = (sBallDiameter * mMetersToPixelsY + 0.5f).toInt()
-    mParticleSystem = ParticleSystem(this, mDstWidth, mDstHeight)//, mHorizontalBound, mVerticalBound)
+    dstWidth = (DIAMETER * metersToPixelsX + 0.5f).toInt()
+    dstHeight = (DIAMETER * metersToPixelsY + 0.5f).toInt()
+    particleSystem = ParticleSystem(this, dstWidth, dstHeight)//, mHorizontalBound, mVerticalBound)
     val opts = BitmapFactory.Options()
     opts.inDither = true
     opts.inPreferredConfig = Bitmap.Config.RGB_565
   }
 
   override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-    mXOrigin = (w - mDstWidth) * 0.5f
-    mYOrigin = (h - mDstHeight) * 0.5f
-    mHorizontalBound = (w / mMetersToPixelsX - sBallDiameter) * 0.5f
-    mVerticalBound = (h / mMetersToPixelsY - sBallDiameter) * 0.5f
+    xOrigin = (w - dstWidth) * 0.5f
+    yOrigin = (h - dstHeight) * 0.5f
+    horizontalBound = (w / metersToPixelsX - DIAMETER) * 0.5f
+    verticalBound = (h / metersToPixelsY - DIAMETER) * 0.5f
   }
 
   override fun onSensorChanged(event: SensorEvent) {
     if (event.sensor.type != Sensor.TYPE_ACCELEROMETER) return
     when (mDisplay?.rotation) {
       Surface.ROTATION_0 -> {
-        mSensorX = event.values[0]
-        mSensorY = event.values[1]
+        sensorX = event.values[0]
+        sensorY = event.values[1]
       }
       Surface.ROTATION_90 -> {
-        mSensorX = -event.values[1]
-        mSensorY = event.values[0]
+        sensorX = -event.values[1]
+        sensorY = event.values[0]
       }
       Surface.ROTATION_180 -> {
-        mSensorX = -event.values[0]
-        mSensorY = -event.values[1]
+        sensorX = -event.values[0]
+        sensorY = -event.values[1]
       }
       Surface.ROTATION_270 -> {
-        mSensorX = event.values[1]
-        mSensorY = -event.values[0]
+        sensorX = event.values[1]
+        sensorY = -event.values[0]
       }
     }
-    //println("@@@@ ${mSensorX}, $mSensorY")
   }
 
-  override fun onDraw(canvas: Canvas?) {
-    val particleSystem = mParticleSystem
-    val now = System.currentTimeMillis()
-    val sx = mSensorX
-    val sy = mSensorY
-    //particleSystem.update(sx, sy, now)
-    particleSystem.update(sx, sy, now, mHorizontalBound, mVerticalBound)
-    val xc = mXOrigin
-    val yc = mYOrigin
-    val xs = mMetersToPixelsX
-    val ys = mMetersToPixelsY
-    val count = particleSystem.getParticleCount()
-    for (i in 0 until count) {
-      val x = xc + particleSystem.getPosX(i) * xs
-      val y = yc - particleSystem.getPosY(i) * ys
-      //println("@@@ $i $x, $y")
-      particleSystem.mBalls[i]?.translationX = x
-      particleSystem.mBalls[i]?.translationY = y
-      //println("@@@ $x, $y")
+  override fun onDraw(canvas: Canvas?) = with (particleSystem) {
+    update(sensorX, sensorY, System.currentTimeMillis(), horizontalBound, verticalBound)
+    balls.forEach { ball ->
+      ball.apply {
+        translationX = xOrigin + posX * metersToPixelsX
+        translationY = yOrigin - posY * metersToPixelsY
+      }
     }
     invalidate()
   }
 
-  override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+  override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) = Unit
 
   companion object {
-    const val sBallDiameter = 0.004f
-    const val sBallDiameter2 = sBallDiameter * sBallDiameter
+    const val DIAMETER = 0.004f
   }
+
 }
