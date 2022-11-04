fd7eb968ba772e1931b3fd925901170f82cb7a99
benigumocom <benigumo.com@gmail.com>
2022-11-04 20:52:19 +0900

    Add Test

--- app/src/main/java/com/template/ea/BallSystem.kt
+++ app/src/main/java/com/template/ea/BallSystem.kt
@@ -54,8 +54,8 @@ class BallSystem(view: MainView, width: Int, height: Int) {
             var d2 = dx.pow(2) + dy.pow(2)
 
             if (d2 <= MainView.DIAMETER.pow(2)) { // collision
-              dx += (Math.random().toFloat() / 2f) / 10000f
-              dy += (Math.random().toFloat() / 2f) / 10000f
+              dx += (Math.random().toFloat() / 2) / 10000
+              dy += (Math.random().toFloat() / 2) / 10000
               d2 = dx.pow(2) + dy.pow(2)
 
               val d = sqrt(d2.toDouble()).toFloat()
