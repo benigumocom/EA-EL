db9092d777a1995e5a508fe136efe6c37a335e5e
benigumocom <benigumo.com@gmail.com>
2022-11-04 16:18:51 +0900

    Configuration screen mode

--- app/src/main/java/com/template/ea/MainActivity.kt
+++ app/src/main/java/com/template/ea/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.template.ea
 
+import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
 import android.os.Bundle
-import android.os.PowerManager
-import android.os.PowerManager.WakeLock
+import android.view.View
+import android.view.WindowInsets
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.getSystemService
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 // https://github.com/googlearchive/android-AccelerometerPlay/blob/master/app/src/main/java/com/example/android/accelerometerplay/AccelerometerPlayActivity.java
 
@@ -12,8 +15,13 @@ class MainActivity : AppCompatActivity() {
 
   private lateinit var playView: MainView
 
+  @SuppressLint("SourceLockedOrientationActivity")
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+    WindowInsetsControllerCompat(window, window.decorView)
+      .hide(WindowInsetsCompat.Type.systemBars())
+    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
     playView = MainView(this).apply {
       setBackgroundResource(R.drawable.wood)
